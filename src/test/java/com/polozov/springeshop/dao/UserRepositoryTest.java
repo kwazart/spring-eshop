571c8ced30a5fa5b4abbb50c90b7c449375407ea
Artem Polozov <artdd52669@gmail.com>
2021-06-27 19:39:15 +0300

    20. lesson-07 (example of test with init sql script)

--- src/test/java/com/polozov/springeshop/dao/UserRepositoryTest.java
+++ src/test/java/com/polozov/springeshop/dao/UserRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.polozov.springeshop.dao;
 
+import com.polozov.springeshop.domain.Role;
 import com.polozov.springeshop.domain.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -7,10 +8,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
+@SqlGroup({@Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:initUsers.sql")})
 class UserRepositoryTest {
     @Autowired
     private TestEntityManager entityManager;
@@ -36,6 +40,21 @@ class UserRepositoryTest {
         Assertions.assertEquals(user.getName(), actualUser.getName());
         Assertions.assertEquals(user.getPassword(), actualUser.getPassword());
         Assertions.assertEquals(user.getEmail(), actualUser.getEmail());
+
+    }
+
+    @Test
+    void checkFindByNameAfterSql() {
+        //execute
+        User actualUser = userRepository.findFirstByName("admin");
+
+        //check
+        Assertions.assertNotNull(actualUser);
+        Assertions.assertEquals(5, actualUser.getId());
+        Assertions.assertEquals("admin", actualUser.getName());
+        Assertions.assertEquals("adminpass", actualUser.getPassword());
+        Assertions.assertEquals("admin@mail.ru", actualUser.getEmail());
+        Assertions.assertEquals(Role.ADMIN, actualUser.getRole());
 
     }
 }
---
+++ src/test/resources/initUsers.sql
@@ -0,0 +1,11 @@
+INSERT INTO users
+(id, archive, email, name, password, role)
+VALUES (3, false, 'petr@mail.ru', 'petr', 'pass', 'CLIENT');
+
+INSERT INTO users
+(id, archive, email, name, password, role)
+VALUES (4, false, 'tom@mail.ru', 'tom', 'pass', 'MANAGER');
+
+INSERT INTO users
+(id, archive, email, name, password, role)
+VALUES (5, false, 'admin@mail.ru', 'admin', 'adminpass', 'ADMIN');
